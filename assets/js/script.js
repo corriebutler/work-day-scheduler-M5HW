73b63e09087bc20d7b245291d9166c9f04532bf6
Corrie Butler <corriebutler12@gmail.com>
2021-10-12 15:17:58 -0600

    added saving the task details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+// Declaring variables
+
+// Audit Tasks
+
+// Load the tasks back in
+
+
+// Save task descriptions
+$('.saveBtn').on('click', function savingTasks() {
+
+    var tasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
+    var taskDescriptions = $(this).siblings('.description').val();
+    var hourSlot = $(this).siblings('.hour').text().trim();
+    
+    console.log(hourSlot);
+    var taskSave = {
+        details: taskDescriptions,
+        hour: hourSlot
+    };
+
+    tasks.push(taskSave);
+
+    localStorage.setItem("savedTasks", JSON.stringify(tasks));
+});
+
 // Main Date and Time in Header
 var currentTime = moment().format('MMMM Do, YYYY - hh:mm a');
 $('#currentDay')
@@ -6,6 +31,6 @@ setInterval(function() {
     $('#currentDay').text(moment().format('MMMM Do, YYYY - hh:mm a'));
 }, 45000);
 
-
+// Load the save button icon
 $('.saveBtn')
     .html('<i class="far fa-save fa-lg"></i>')
